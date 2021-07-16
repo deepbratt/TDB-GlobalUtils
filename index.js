89bb7e13a770ade368280f2be3edb8afb3d4dd2f
Hamza Ali <56255291+hamzasaiff195@users.noreply.github.com>
2021-07-16 14:55:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 exports.catchAsync = require('./errorHandling/catchAsync');
 exports.AppError = require('./errorHandling/AppError');
-exports.passportGoogle = require('./globalUtils/passportGoogle');
-exports.passportFacebook = require('./globalUtils/passportFacebook');
+
