4eddb4fe39ba7a42a34b26018029e7fe7a830c79
Hamza Saif <hamzasaiff@gmail.com>
2021-07-16 14:45:59 +0500

    added facebook and google passport strategy in globalUtils

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-exports.catchAsync=require('./errorHandling/catchAsync');
-exports.AppError=require('./errorHandling/AppError')
+exports.catchAsync = require('./errorHandling/catchAsync');
+exports.AppError = require('./errorHandling/AppError');
+exports.passportGoogle = require('./globalUtils/passportGoogle');
+exports.passportFacebook = require('./globalUtils/passportFacebook');
