21ca8c06717756e61374c08cf63107022959d7a0
Hamza Saif <hamzasaiff@gmail.com>
2021-07-16 16:22:24 +0500

    Added JWT Management

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.catchAsync = require('./errorHandling/catchAsync');
 exports.AppError = require('./errorHandling/AppError');
 exports.errorHandler = require('./errorHandling/errorHandler');
 exports.appConstants = require('./constants/appConstants');
+exports.jwtManagement = require('./jwt/jwtManagement');
