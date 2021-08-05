093c2ccd49bd29cbec78ffdf82edbfc9348cf2f5
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-05 08:32:36 +0500

    updated

--- fileUpload/s3.js
+++ fileUpload/s3.js
@@ -2,20 +2,19 @@ const S3 = require('aws-sdk/clients/s3');
 const { v4: uuidv4 } = require('uuid');
 const AppError = require('../errorHandling/AppError');
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+// const bucketName = process.env.AWS_BUCKET_NAME;
+// const region = process.env.AWS_BUCKET_REGION;
+// const accessKeyId = process.env.AWS_ACCESS_KEY;
+// const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-const s3 = new S3({
-	region,
-	accessKeyId,
-	secretAccessKey,
-});
-
-const uploadS3 = (file) => {
+const uploadS3 = (file, region, accessKeyId, secretAccessKey, bucketName) => {
 	let myFile = file.originalname.split('.');
 	const ext = myFile[myFile.length - 1];
+	const s3 = new S3({
+		region,
+		accessKeyId,
+		secretAccessKey,
+	});
 	const uploadParams = {
 		Bucket: bucketName,
 		Body: file.buffer,
