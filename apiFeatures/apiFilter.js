3a92e2b7de68fb2008a86bd9d665dd14bc796f35
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-14 05:57:17 +0500

    updated

--- apiFeatures/apiFilter.js
+++ apiFeatures/apiFilter.js
@@ -11,7 +11,7 @@ class APIFeatures {
 
 		// FILTER MONGOOSE OPERATORS
 		let queryStr = JSON.stringify(queryParams);
-		queryStr = queryStr.replace(/\b(gte|gt|lte|lt|regex)\b/g, (match) => `$${match}`);
+		queryStr = queryStr.replace(/\b(gte|gt|lte|lt|regex|options)\b/g, (match) => `$${match}`);
 
 		console.log(JSON.parse(queryStr));
 
