1fabb5595c865bd1fc45e96d1858f73e8c63f4a3
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-28 10:43:49 +0500

    updated

--- apiFeatures/apiFilter.js
+++ apiFeatures/apiFilter.js
@@ -59,9 +59,9 @@ class APIFeatures {
 	search() {
 		if (this.queryParams.keyword) {
 			this.query = this.query.find(
-				{ $match: { $text: { $search: this.queryParams.keyword } } },
-				{ $project: { score: { $meta: 'textScore' } } },
-				{ $match: { score: { $gt: 1.0 } } }
+				{ $text: { $search: this.queryParams.keyword } },
+				{ field: { score: { $meta: 'textScore' } } },
+				{ score: { $gt: 1.0 } }
 			);
 		}
 		console.log(this.query);
