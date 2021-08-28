de3aab0749f200ade0850989b72691fa1e86779f
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-08-28 10:15:17 +0500

    updated

--- apiFeatures/apiFilter.js
+++ apiFeatures/apiFilter.js
@@ -58,7 +58,7 @@ class APIFeatures {
 
 	search() {
 		if (this.queryParams.keyword) {
-			this.query = this.query.aggregate([
+			this.query = this.query.find([
 				{ $match: { $text: { $search: this.queryParams.keyword } } },
 				{ $project: { score: { $meta: 'textScore' } } },
 				{ $match: { score: { $gt: 1.0 } } },
