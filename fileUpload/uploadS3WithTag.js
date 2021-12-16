009db7675c1e463be799d192a55629388928a06d
Hamza Saif <hamzasaiff@gmail.com>
2021-12-16 13:37:35 +0500

    added file to upload image to s3 bucket with temporary tag

---
+++ fileUpload/uploadS3WithTag.js
@@ -0,0 +1,30 @@
+const S3 = require('aws-sdk/clients/s3');
+const { v4: uuidv4 } = require('uuid');
+
+const s3WithTag = (file, region, accessKeyId, secretAccessKey, bucketName) => {
+  let myFile = file.originalname.split('.');
+  const ext = myFile[myFile.length - 1];
+  const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+  });
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: file.buffer,
+    Key: `${uuidv4()}.${ext}`,
+  };
+
+  return s3
+    .upload(uploadParams, {
+      tags: [
+        {
+          Key: 'imageType',
+          Value: 'Temporary',
+        },
+      ],
+    })
+    .promise();
+};
+
+module.exports = s3WithTag;
